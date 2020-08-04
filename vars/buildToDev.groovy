c9eef673b0db89559eb9c0332e0151e1acd51e50
hippyod <hippyod@yahoo.com>
2020-08-04 15:23:27 -0500

    starting 2nd, better take on building to sandboxes

--- vars/buildToDev.groovy
+++ vars/buildToDev.groovy
@@ -25,6 +25,12 @@ void call(Map args) {
                                   "BUILDS MAY ONLY DEPLOY TO ONE OF THE FOLLOWING NAMESPACES:",
                                   "${projectInfo.devNamespace} ${sboxNamepaces}")
     }
+    
+    projectInfo.imageTag = projectInfo.devEnv
+    if (projectInfo.deployToNamespace.contains(el.cicd.SANDBOX_NAMESPACE_BADGE)) {
+        def index = projectInfo.deployToNamespace.split('-').last()
+        projectInfo.imageTag = "${el.cicd.SANDBOX_NAMESPACE_BADGE}-${index}"
+    }
 
     stage('Checkout code from repository') {
         pipelineUtils.echoBanner("CLONING ${microService.gitRepoName} REPO, REFERENCE: ${microService.gitBranch}")
@@ -72,21 +78,23 @@ void call(Map args) {
     stage('build image and push to repository') {
         def imageRepo = el.cicd["${projectInfo.DEV_ENV}_IMAGE_REPO"]
         def pullSecret = el.cicd["${projectInfo.DEV_ENV}_IMAGE_REPO_PULL_SECRET"]
+        def buildConfigName = "${microService.id}-${projectInfo.imageTag}"
+        
         dir(microService.workDir) {
             sh """
                 ${pipelineUtils.shellEchoBanner("BUILD ARTIFACT AND PUSH TO ARTIFACT REPOSITORY")}
 
-                if [[ ! -n `oc get bc ${microService.id} -n ${projectInfo.nonProdCicdNamespace} --ignore-not-found` ]]
+                if [[ ! -n `oc get bc ${buildConfigName} -n ${projectInfo.nonProdCicdNamespace} --ignore-not-found` ]]
                 then
-                    oc new-build --name ${microService.id} \
+                    oc new-build --name ${buildConfigName} \
                                  --binary=true \
                                  --strategy=docker \
                                  --to-docker \
-                                 --to=${imageRepo}/${microService.id}:${projectInfo.deployToEnv} \
+                                 --to=${imageRepo}/${microService.id}:${projectInfo.imageTag} \
                                  --push-secret=${pullSecret} \
                                  -n ${projectInfo.nonProdCicdNamespace}
 
-                    oc set build-secret --pull bc/${microService.id} ${pullSecret} -n ${projectInfo.nonProdCicdNamespace}
+                    oc set build-secret --pull bc/${buildConfigName} ${pullSecret} -n ${projectInfo.nonProdCicdNamespace}
                 fi
 
                 chmod 777 Dockerfile
@@ -95,7 +103,7 @@ void call(Map args) {
                 echo "\nLABEL SRC_COMMIT_HASH='${microService.srcCommitHash}'" >> Dockerfile
                 echo "\nLABEL EL_CICD_BUILD_TIME='\$(date +%d.%m.%Y-%H.%M.%S%Z)'" >> Dockerfile
 
-                oc start-build ${microService.id} --from-dir=. --wait --follow -n ${projectInfo.nonProdCicdNamespace}
+                oc start-build ${buildConfigName} --from-dir=. --wait --follow -n ${projectInfo.nonProdCicdNamespace}
             """
         }
     }
