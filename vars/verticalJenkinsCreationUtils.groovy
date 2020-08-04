3b305eec324348d534c14f68fd69a4383a67c6f9
hippyod <hippyod@yahoo.com>
2020-08-03 19:11:00 -0500

    programming error

--- vars/verticalJenkinsCreationUtils.groovy
+++ vars/verticalJenkinsCreationUtils.groovy
@@ -62,8 +62,9 @@ def refreshSharedPipelines(def projectInfo, def isNonProd) {
                 'production-manifest-pipeline-template.yml redeploy-release-candidate-pipeline-template.yml ' +
                 'build-and-deploy-microservices-pipeline-template.yml' : 
                 'deploy-to-production-pipeline-template.yml'
-
-        pipelineUtils.echoBanner('CREATING SHARED PIPELINES:', *templates.split(' '))
+                
+        def msgs = ['CREATING SHARED PIPELINES:'].addAll(templates.split(' '))
+        pipelineUtils.echoBanner(*msgs)
         
         def namespace = isNonProd ? projectInfo.nonProdCicdNamespace : projectInfo.prodCicdNamespace
         
