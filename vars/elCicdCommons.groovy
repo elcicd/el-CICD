8aff1c66a2198bbc0568dded3b5c468f69f8d064
hippyod <hippyod@yahoo.com>
2020-12-02 14:05:15 -0600

    programming error

--- vars/elCicdCommons.groovy
+++ vars/elCicdCommons.groovy
@@ -14,12 +14,6 @@ def initialize() {
         el.cicd.BUILDER_STEPS_DIR = "${el.cicd.PROJECT_INFO_DIR}/builder-steps"
         el.cicd.PROJECT_DEFS_DIR = "${el.cicd.PROJECT_INFO_DIR}/project-defs"
 
-        dir (el.cicd.PROJECT_INFO_DIR) {
-            git url: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY,
-                branch: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY_BRANCH_NAME,
-                credentialsId: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY_READ_ONLY_GITHUB_PRIVATE_KEY_ID
-        }
-
         el.cicd.TEMP_DIR="/tmp/${BUILD_TAG}"
         sh """
             rm -rf ${WORKSPACE}/*
@@ -36,5 +30,11 @@ def initialize() {
         el.cicd.RELEASE_VERSION_PREFIX = 'v'
 
         el.cicd = el.cicd.asImmutable()
+
+        dir (el.cicd.PROJECT_INFO_DIR) {
+            git url: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY,
+                branch: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY_BRANCH_NAME,
+                credentialsId: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY_READ_ONLY_GITHUB_PRIVATE_KEY_ID
+        }
     }
 }
