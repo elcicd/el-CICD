58d873fefb942c306234b5db521e06be50398148
hippyod <hippyod@yahoo.com>
2020-12-04 17:49:18 -0600

    testing

--- vars/elCicdNode.groovy
+++ vars/elCicdNode.groovy
@@ -55,7 +55,7 @@ def call(Map args, Closure body) {
                 pipelineUtils.echoBanner("ON-FAIL-_FILE SEARCH: ${onFailScriptFile}")
                 if (onFailScriptFile) {
                     pipelineUtils.echoBanner("LOADING SCRIPT!!!")
-                    onFailScript = load onFailScriptFile[0].path
+                    onFailScript = load "${onFailScriptFile[0].path}"
                     pipelineUtils.echoBanner("SCRIPT LOADED: ${onFailScript}!!!")
                 }
             }
