554d5aed7997c6689df6c68f5f35fcbfc9c49712
hippyod <hippyod@yahoo.com>
2021-01-08 21:43:06 -0600

    creating new OKD 4.x agents and build structure

--- vars/createAllJenkinsAgents.groovy
+++ vars/createAllJenkinsAgents.groovy
@@ -17,6 +17,16 @@ def call(Map args) {
 
         pipelineUtils.echoBanner('CREATE JENKINS AGENTS (in the following order):', agentNames.join(', '))
 
+        sh """
+            if [[ -z \$(oc get is --ignore-not-found ${el.cicd.JENKINS_AGENT_IMAGE_PREFIX}-${el.cicd.JENKINS_AGENT_DEFAULT} -n openshift) ]]
+            then
+                echo "FROM ${OCP_IMAGE_REPO}/jenkins-agent-base" | oc new-build -D - --name ${el.cicd.JENKINS_AGENT_IMAGE_PREFIX}-${el.cicd.JENKINS_AGENT_DEFAULT} -n openshift
+                sleep 10
+                oc logs -f bc/${el.cicd.JENKINS_AGENT_IMAGE_PREFIX}-${el.cicd.JENKINS_AGENT_DEFAULT} -n openshift
+                oc delete bc ${el.cicd.JENKINS_AGENT_IMAGE_PREFIX}-${el.cicd.JENKINS_AGENT_DEFAULT} -n openshift
+            fi
+        """
+
         dir(el.cicd.JENKINS_CONFIG_DIR) {
             agentNames.each { agentName ->
                 sh """
