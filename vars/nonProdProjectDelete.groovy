9b0294f7b9fd1ee6143f0677c650af808a59f002
hippyod <hippyod@yahoo.com>
2021-01-15 21:14:02 -0600

    minor change

--- vars/nonProdProjectDelete.groovy
+++ vars/nonProdProjectDelete.groovy
@@ -7,27 +7,16 @@
 def call(Map args) {
     def projectInfo = args.projectInfo
 
-    stage('Remove stale namespace environments and pipelines if necessary') {
+    stage('Remove project namespace environments') {
         def namespacesToDelete = projectInfo.nonProdNamespaces.values().join(' ')
         namespacesToDelete += projectInfo.sandboxNamespaces ? ' ' + projectInfo.sandboxNamespaces.join(' ') : ''
         namespacesToDelete += args.deleteRbacGroupJenkins ? " ${projectInfo.nonProdCicdNamespace}" : ''
 
+        def msg = args.deleteRbacGroupJenkins ?
+            "REMOVING ${projectInfo.rbacGroup} AUTOMATION SERVER AND ${projectInfo.id} NON-PROD ENVIRONMENT(S)" :
+            "REMOVING ${projectInfo.id} NON-PROD ENVIRONMENTS" :
+
         sh """
-            ${pipelineUtils.shellEchoBanner("REMOVING PROJECT PIPELINES FOR ${projectInfo.id}, IF ANY")}
-
-            if [[ '${args.deleteRbacGroupJenkins}' != 'true' ]]
-            then
-                for BCS in \$(oc get bc -l projectid=${projectInfo.id} -n ${projectInfo.nonProdCicdNamespace} -o jsonpath='{.items[*].metadata.name}')
-                do
-                    while [ \$(oc get bc \${BCS} -n ${projectInfo.nonProdCicdNamespace} | grep \${BCS} | wc -l) -gt 0 ] ;
-                    do
-                        oc delete bc \${BCS} --ignore-not-found -n ${projectInfo.nonProdCicdNamespace}
-                        sleep 5
-                        ${shellEcho ''}
-                    done
-                done
-            fi
-
             ${pipelineUtils.shellEchoBanner("REMOVING PROJECT NON-PROD ENVIRONMENT(S) FOR ${projectInfo.id}")}
 
             oc delete project ${namespacesToDelete} || true
@@ -48,21 +37,35 @@ def call(Map args) {
         onboardingUtils.deleteOldGithubKeys(projectInfo, true)
     }
 
-    stage('Delete GitHub deploy keys from Jenkins') {
-        def cicdRbacGroupJenkinsCredsUrls = verticalJenkinsCreationUtils.buildCicdJenkinsUrls(projectInfo)
+    if (!args.deleteRbacGroupJenkins) {
+        stage('Remove project build-to-dev pipelines from Jenkins') {
+            def namespacesToDelete = projectInfo.nonProdNamespaces.values().join(' ')
+            namespacesToDelete += projectInfo.sandboxNamespaces ? ' ' + projectInfo.sandboxNamespaces.join(' ') : ''
+            namespacesToDelete += args.deleteRbacGroupJenkins ? " ${projectInfo.nonProdCicdNamespace}" : ''
 
-        onboardingUtils.init()
-        def authBearerCommand = """cat ${el.cicd.TEMPLATES_DIR}/AuthBearerHeader-template.txt | sed "s/%TOKEN%/\$(oc whoami -t)/" > ${el.cicd.TEMP_DIR}/AuthBearerHeader.txt"""
-        sh """
-            ${shellEcho 'Creating header file with auth token'}
-            ${maskCommand(authBearerCommand)}
-        """
-
-        projectInfo.microServices.each { microService ->
             sh """
-                curl -ksS -X POST -H "`cat ${el.cicd.TEMP_DIR}/AuthBearerHeader.txt`" \
-                    "${cicdRbacGroupJenkinsCredsUrls.updateProdCicdJenkinsCredsUrl}/${microService.gitSshPrivateKeyName}/doDelete"
+                ${pipelineUtils.shellEchoBanner("REMOVING PROJECT BUILD-TO-DEV PIPELINES FOR ${projectInfo.id}")}
+
+                oc delete bc -l projectid=${projectInfo.id} -n ${projectInfo.nonProdCicdNamespace}
             """
+        }
+
+        stage('Delete GitHub deploy keys from Jenkins') {
+            def cicdRbacGroupJenkinsCredsUrls = verticalJenkinsCreationUtils.buildCicdJenkinsUrls(projectInfo)
+
+            onboardingUtils.init()
+            def authBearerCommand = """cat ${el.cicd.TEMPLATES_DIR}/AuthBearerHeader-template.txt | sed "s/%TOKEN%/\$(oc whoami -t)/" > ${el.cicd.TEMP_DIR}/AuthBearerHeader.txt"""
+            sh """
+                ${shellEcho 'Creating header file with auth token'}
+                ${maskCommand(authBearerCommand)}
+            """
+
+            projectInfo.microServices.each { microService ->
+                sh """
+                    curl -ksS -X POST -H "`cat ${el.cicd.TEMP_DIR}/AuthBearerHeader.txt`" \
+                        "${cicdRbacGroupJenkinsCredsUrls.updateProdCicdJenkinsCredsUrl}/${microService.gitSshPrivateKeyName}/doDelete"
+                """
+            }
         }
     }
 }
