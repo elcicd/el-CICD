0858ce4af40964137a80b101d7bd7263dd277004
hippyod <hippyod@yahoo.com>
2021-01-20 17:45:12 -0600

    testing

--- vars/refreshCredentials.groovy
+++ vars/refreshCredentials.groovy
@@ -18,7 +18,8 @@ def call(Map args) {
 
         allProjectFiles.each { projectFile ->
             echo "projectFile ${projectFile.name}"
-            def projectId = projectFile.name.toString().replaceAll('[.](yml||yaml|json|js)', '')​
+            def projectId = projectFile.name
+            def projectId = projectId.toString().replaceAll('[.](yml||yaml|json|js)', '')​
             echo 'howdy1'
             def projectInfo = pipelineUtils.gatherProjectInfoStage(projectId)
             echo 'howdy2'
