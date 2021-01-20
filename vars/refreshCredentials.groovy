f6b56fb666288f8050924eb68aa3440b3a298311
hippyod <hippyod@yahoo.com>
2021-01-20 17:29:27 -0600

    programming error

--- vars/refreshCredentials.groovy
+++ vars/refreshCredentials.groovy
@@ -12,8 +12,10 @@ def call(Map args) {
             sh(returnStdout: true, script: /find .\/ -type f -iname *.json -o -iname *.js -o -iname *.yml -o -iname *.yml/)
         allProjectFiles = allProjectFiles.split('\n')
 
+        echo "allProjectFiles ${allProjectFiles}"
+
         allProjectFiles.each { projectFile ->
-            def projectId = projectFile.name.replaceAll('[.](yml||yaml|json|js)', '')​
+            def projectId = projectFile.replaceAll('[.](yml||yaml|json|js)', '')​
             def projectInfo = pipelineUtils.gatherProjectInfoStage(projectId)
             def envs = args.isNonProd ? projectInfo.NON_PROD_ENVS : [projectInfo.PRE_PROD_ENV, projectInfo.PROD_ENV]
 
