315245739bf31978cdf763d6f22516c7b9eca134
hippyod <hippyod@yahoo.com>
2021-02-02 11:51:44 -0600

    programming error

--- vars/credentialUtils.groovy
+++ vars/credentialUtils.groovy
@@ -30,7 +30,7 @@ def copyElCicdMetaInfoBuildAndPullSecretsToGroupCicdServer(def projectInfo, def 
             oc apply -f - -n ${projectInfo.cicdMasterNamespace}
 
         BUILD_SECRETS_NAME=${el.cicd.EL_CICD_BUILD_SECRETS_NAME}
-        if [[ ! -z ${BUILD_SECRETS_NAME} ]]
+        if [[ ! -z \${BUILD_SECRETS_NAME} ]]
         then
             oc get secret \${BUILD_SECRETS_NAME} -o yaml -n ${el.cicd.ONBOARDING_MASTER_NAMESPACE} | \
                 ${el.cicd.CLEAN_K8S_RESOURCE_COMMAND} | \
