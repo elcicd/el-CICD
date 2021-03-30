c4fefb5e7c260985dfa5bf0305b17952ebb98903
hippyod <hippyod@yahoo.com>
2021-03-30 14:02:03 -0500

    testing

--- vars/buildToDev.groovy
+++ vars/buildToDev.groovy
@@ -79,7 +79,7 @@ void call(Map args) {
 
         dir(microService.workDir) {
             sh """
-                HAS_BC=$(oc get bc --no-headers --ignore-not-found ${buildConfigName} -n ${projectInfo.cicdMasterNamespace} | tr -d '[:space:]')
+                HAS_BC=\$(oc get bc --no-headers --ignore-not-found ${buildConfigName} -n ${projectInfo.cicdMasterNamespace} | tr -d '[:space:]')
                 if [[ -z \${HAS_BC} ]]
                 then
                     oc new-build --name ${buildConfigName} \
