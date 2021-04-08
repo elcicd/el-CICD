108a16672aa4c685320451d25a5bb92b020e592b
hippyod <hippyod@yahoo.com>
2021-04-08 18:24:30 -0500

    programming error

--- vars/projectDeletion.groovy
+++ vars/projectDeletion.groovy
@@ -27,7 +27,7 @@ def call(Map args) {
 
             COUNTER=1
             until
-                -z $(oc get projects --ignore-not-found \${NAMESPACE})
+                -z \$(oc get projects --ignore-not-found \${NAMESPACE})
             do
                 printf "%0.s-" \$(seq 1 \${COUNTER})
                 echo
