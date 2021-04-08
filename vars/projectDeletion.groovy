1f2f2281f24ed786544b2aba01ad1f25f9452b9a
hippyod <hippyod@yahoo.com>
2021-04-08 18:35:42 -0500

    programming error

--- vars/projectDeletion.groovy
+++ vars/projectDeletion.groovy
@@ -27,8 +27,7 @@ def call(Map args) {
 
             set +x
             COUNTER=1
-            until
-                -z \$(oc get projects --no-headers --ignore-not-found ${namespacesToDelete})
+            until [[ -z \$(oc get projects --no-headers --ignore-not-found ${namespacesToDelete}) ]]
             do
                 printf "%0.s-" \$(seq 1 \${COUNTER})
                 echo
