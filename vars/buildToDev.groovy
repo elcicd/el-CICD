102710367714823eb08e5c32f0a9dfaa3b52a1ca
hippyod <hippyod@yahoo.com>
2022-01-12 17:09:21 -0600

    first start at building an image directly in the agent using podman

--- vars/buildToDev.groovy
+++ vars/buildToDev.groovy
@@ -98,7 +98,7 @@ void call(Map args) {
                 echo "\nLABEL SRC_COMMIT_HASH='${microService.srcCommitHash}'" >> Dockerfile
                 echo "\nLABEL EL_CICD_BUILD_TIME='\$(date +%d.%m.%Y-%H.%M.%S%Z)'" >> Dockerfile
 
-                buildah bud -f ./Dockerfile \
+                podman unshare buildah bud -f ./Dockerfile \
                     --build-arg=EL_CICD_BUILD_SECRETS_NAME=./${el.cicd.EL_CICD_BUILD_SECRETS_NAME} \
                     -t ${imageRepo}/${microService.id}:${projectInfo.imageTag}
 
