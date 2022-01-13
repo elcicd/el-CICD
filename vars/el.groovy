d95342993001e734889086a1883364f7dc14237c
hippyod <hippyod@yahoo.com>
2022-01-13 12:13:24 -0600

    first start at building an image directly in the agent using podman

--- vars/el.groovy
+++ vars/el.groovy
@@ -60,7 +60,7 @@ def node(Map args, Closure body) {
                 resourceLimitMemory: "${el.cicd.JENKINS_AGENT_MEMORY_LIMIT}",
                 resourceRequestCpu: "${el.cicd.JENKINS_AGENT_CPU_REQUEST}",
                 resourceLimitCpu: "${el.cicd.JENKINS_AGENT_CPU_LIMIT}",
-                runAsUser: 'jenkins'
+                runAsUser: '1001'
             )
         ],
         volumes: secretVolume
