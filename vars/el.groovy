280f184e0db52033765312f7eff3b15c0960725f
hippyod <hippyod@yahoo.com>
2022-01-14 18:06:31 -0600

    first start at building an image directly in the agent using podman

--- vars/el.groovy
+++ vars/el.groovy
@@ -50,7 +50,7 @@ def node(Map args, Closure body) {
         serviceAccount: 'jenkins',
         podRetention: onFailure(),
         idleMinutes: "${el.cicd.JENKINS_AGENT_MEMORY_IDLE_MINUTES}",
-        runAsUser: '1002',
+        runAsUser: '1001',
         containers: [
             containerTemplate(
                 name: 'jnlp',
