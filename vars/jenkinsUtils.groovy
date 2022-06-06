6c0f9595f5aa5fbaaa0f970b7cc23ef650e98d5b
hippyod <hippyod@yahoo.com>
2022-06-06 12:36:01 -0500

    programming error

--- vars/jenkinsUtils.groovy
+++ vars/jenkinsUtils.groovy
@@ -121,14 +121,14 @@ def copyElCicdCredentialsToCicdServer(def projectInfo, def ENVS) {
     def keyId = el.cicd.EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID
     loggingUtils.echoBanner("PUSH ${keyId} CREDENTIALS TO CICD SERVER")
     withCredentials([sshUserPrivateKey(credentialsId: keyId, keyFileVariable: 'EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')]) {
-        EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID = EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID.stripTrailing()
+        EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID = EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID.trim()
         pushSshCredentialsToJenkins(projectInfo, keyId, 'EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')
     }
 
     keyId = el.cicd.EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID
     loggingUtils.echoBanner("PUSH ${keyId} CREDENTIALS TO CICD SERVER")
     withCredentials([sshUserPrivateKey(credentialsId: keyId, keyFileVariable: 'EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')]) {
-        EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID = EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID.stripTrailing()
+        EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID = EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID.trim()
         pushSshCredentialsToJenkins(projectInfo, keyId, 'EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')
     }
 
