47e3200d47da06e728ead7ff90485988146428cf
hippyod <hippyod@yahoo.com>
2022-06-06 03:58:27 -0500

    programming error

--- vars/jenkinsUtils.groovy
+++ vars/jenkinsUtils.groovy
@@ -120,14 +120,14 @@ def deletePipeline(def projectInfo, def folderName, def pipelineName) {
 def copyElCicdCredentialsToCicdServer(def projectInfo, def ENVS) {
     def keyId = el.cicd.EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID
     loggingUtils.echoBanner("PUSH ${keyId} CREDENTIALS TO CICD SERVER")
-    withCredentials([sshUserPrivateKey(credentialsId: keyId, keyFileVariable: "${keyId}")]) {
-        pushSshCredentialsToJenkins(projectInfo, keyId,"${keyId}")
+    withCredentials([sshUserPrivateKey(credentialsId: keyId, keyFileVariable: 'EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')]) {
+        pushSshCredentialsToJenkins(projectInfo, keyId, 'EL_CICD_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')
     }
 
     keyId = el.cicd.EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID
     loggingUtils.echoBanner("PUSH ${keyId} CREDENTIALS TO CICD SERVER")
-    withCredentials([sshUserPrivateKey(credentialsId: keyId, keyFileVariable: "${keyId}")]) {
-        pushSshCredentialsToJenkins(projectInfo, keyId, "${keyId}")
+    withCredentials([sshUserPrivateKey(credentialsId: keyId, keyFileVariable: 'EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')]) {
+        pushSshCredentialsToJenkins(projectInfo, keyId, 'EL_CICD_CONFIG_GIT_REPO_READ_ONLY_GITHUB_PRIVATE_KEY_ID')
     }
 
     def tokenIds = []
@@ -154,6 +154,7 @@ def pushSshCredentialsToJenkins(def projectInfo, def keyId, def sshKeyVar) {
         sh """
             ${shCmd.echo ''}
             set +x
+            echo "sshKeyVar: \${${sshKeyVar}}"
             cp ${el.cicd.TEMPLATES_DIR}/${TEMPLATE_FILE} ${JENKINS_CREDS_FILE}
             sed -i -e 's/%UNIQUE_ID%/${keyId}/g' ${JENKINS_CREDS_FILE}
             JENKINS_CREDS=\$(<${JENKINS_CREDS_FILE})
