9414f7be7337ce8733eadb6805a845bf5d8db14a
hippyod <hippyod@yahoo.com>
2022-07-20 23:03:24 -0500

    programming error

--- vars/deploymentUtils.groovy
+++ vars/deploymentUtils.groovy
@@ -77,8 +77,11 @@ def confirmDeployments(def projectInfo, def microServices) {
     sh """
         for MICROSERVICE_NAME in ${microServiceNames}
         do
-            oc get dc,deploy -l microservice=\${MICROSERVICE_NAME} -o name -n ${projectInfo.deployToNamespace} | \
-                xargs -n1 -t oc rollout status -n ${projectInfo.deployToNamespace}
+            DEPLOYS=$(oc get dc,deploy -l microservice=\${MICROSERVICE_NAME} -o name -n ${projectInfo.deployToNamespace})
+            if [[ ! -z \${DEPLOYS} ]]
+            then
+                echo \${DEPLOYS} | xargs -n1 -t oc rollout status -n ${projectInfo.deployToNamespace}
+            fi
         done
     """
 
