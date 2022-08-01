6c039c7988bcd31d8fd6e727295d1e8e9e5b105c
hippyod <hippyod@yahoo.com>
2022-08-01 03:01:22 -0500

    programming error

--- resources/kustomize/kustomize.sh
+++ resources/kustomize/kustomize.sh
@@ -2,15 +2,15 @@
 set -e +x
 cd "$(dirname ${0})"
 
-cat <&0 > ./resources/all.yml
+cat <&0 > ./resources/all.yaml
 
-awk '/__VALUES_END__/{flag=0} flag; /__VALUES_START__/{flag=1}' ./resources/all.yml > values.yml
-sed -i '/__VALUES_START__/,/__VALUES_END__/d' ./resources/all.yml
+awk '/__VALUES_END__/{flag=0} flag; /__VALUES_START__/{flag=1}' ./resources/all.yaml > values.yaml
+sed -i '/__VALUES_START__/,/__VALUES_END__/d' ./resources/all.yaml
 
-COMMENTS=$(awk '/# EXCLUDED/||/# Profiles:/ {print $0}' ./resources/all.yml)
-RENDERED=$(awk '/# Rendered ->/ {print $0}' ./resources/all.yml)
+COMMENTS=$(awk '/# EXCLUDED/||/# Profiles:/ {print $0}' ./resources/all.yaml)
+RENDERED=$(awk '/# Rendered ->/ {print $0}' ./resources/all.yaml)
 
-helm template kustomize -f values.yml . > kustomization.yml
+helm template kustomize -f values.yaml . > kustomization.yaml
 kustomize build .
 echo "${COMMENTS}"
 echo '---'
