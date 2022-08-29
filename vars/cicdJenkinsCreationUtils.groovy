2339f2d38542437ff21690aeeae99e15613ebd70
hippyod <hippyod@yahoo.com>
2022-08-28 23:07:04 -0500

    programming error

--- vars/cicdJenkinsCreationUtils.groovy
+++ vars/cicdJenkinsCreationUtils.groovy
@@ -40,7 +40,8 @@ def createCicdNamespaceAndJenkins(def projectInfo, def envs) {
             el.cicd.JENKINS_IMAGE_PULL_SECRET =
                 sh(returnStdout: true, script: 'oc get secrets -o custom-columns=:metadata.name | grep deployer-dockercfg')
         }
-
+        
+        def jenkinsUrl = "jenkins-${projectInfo.cicdMasterNamespace}"
         sh """
             ${loggingUtils.shellEchoBanner("CREATING ${projectInfo.cicdMasterNamespace} PROJECT AND JENKINS FOR THE ${projectInfo.rbacGroup} GROUP")}
 
@@ -49,17 +50,17 @@ def createCicdNamespaceAndJenkins(def projectInfo, def envs) {
             ${shCmd.echo ''}
             helm upgrade --install --history-max=0 --cleanup-on-fail --debug \
                 --set elCicdChart.parameters.JENKINS_IMAGE=${el.cicd.JENKINS_IMAGE_REGISTRY}/${el.cicd.JENKINS_IMAGE_NAME} \
-                --set elCicdChart.parameters.JENKINS_URL=${el.cicd.JENKINS_URL} \
+                --set elCicdChart.parameters.JENKINS_URL=${jenkinsUrl} \
                 --set "elCicdChart.parameters.OPENSHIFT_ENABLE_OAUTH='${el.cicd.OKD_VERSION ? 'true' : 'false'}'" \
                 --set elCicdChart.parameters.CPU_LIMIT=${el.cicd.JENKINS_CPU_LIMIT} \
                 --set elCicdChart.parameters.MEMORY_LIMIT=${el.cicd.JENKINS_MEMORY_LIMIT} \
                 --set elCicdChart.parameters.VOLUME_CAPACITY=${el.cicd.JENKINS_VOLUME_CAPACITY} \
                 --set elCicdChart.parameters.JENKINS_IMAGE_PULL_SECRET=${el.cicd.JENKINS_IMAGE_PULL_SECRET} \
-                -n ${el.cicd.ONBOARDING_MASTER_NAMESPACE} \
+                -n ${projectInfo.cicdMasterNamespace} \
                 -f ${el.cicd.JENKINS_HELM_DIR}/values.yml \
                 jenkins \
                 ${el.cicd.JENKINS_HELM_DIR}
-                
+
             ${shCmd.echo ''}
             ${shCmd.echo 'Creating nonrootbuilder SCC if necessary and applying to jenkins ServiceAccount'}
             oc apply -f ${el.cicd.JENKINS_CONFIG_DIR}/jenkinsServiceAccountSecurityContext.yml
