f354f46f02a53a71c672abd5efd7c80da85e7d66
hippyod <hippyod@yahoo.com>
2022-09-08 19:07:17 -0500

    testing

--- vars/jenkinsUtils.groovy
+++ vars/jenkinsUtils.groovy
@@ -148,12 +148,10 @@ def pushSshCredentialsToJenkins(def projectInfo, def keyId, def sshKeyVar) {
         
         sh """
             ${shCmd.echo ''}
-            set -v
             cp ${el.cicd.TEMPLATES_DIR}/${TEMPLATE_FILE} ${JENKINS_CREDS_FILE}
             sed -i -e 's/%UNIQUE_ID%/${keyId}/g' ${JENKINS_CREDS_FILE}
             JENKINS_CREDS=\$(<${JENKINS_CREDS_FILE})
             echo "\${JENKINS_CREDS//%PRIVATE_KEY%/\$(<\${${sshKeyVar}})}" > ${JENKINS_CREDS_FILE}
-            set +v
             
             ${curlCommand} ${projectInfo.jenkinsUrls.CREATE_CREDS}
             ${curlCommand} -f ${projectInfo.jenkinsUrls.UPDATE_CREDS}/${keyId}/config.xml
