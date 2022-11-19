7585be928c7f49d5822d54784af5503d3ff6916a
hippyod <hippyod@yahoo.com>
2022-11-19 16:54:28 -0600

    programming error

--- vars/jenkinsUtils.groovy
+++ vars/jenkinsUtils.groovy
@@ -75,20 +75,11 @@ def pushSshCredentialsToJenkins(def projectInfo, def keyId, def sshKeyEnvVar) {
             cp ${el.cicd.TEMPLATES_DIR}/${TEMPLATE_FILE} ${JENKINS_CREDS_FILE}
             sed -i -e 's/%UNIQUE_ID%/${keyId}/g' ${JENKINS_CREDS_FILE}
             JENKINS_CREDS=\$(<${JENKINS_CREDS_FILE})
-            echo
-            ls -al
-            echo
-            echo 'sshKeyEnvVar: ${sshKeyEnvVar}'
-            echo "sshKeyEnvVar[env]: \${${sshKeyEnvVar}}"
-            echo
-            if [[ -z \${${sshKeyEnvVar}:+x} ]]
+
+            if [[ -f '${sshKeyEnvVar}' ]]
             then
-                echo "foo"
-                sleep 1
                 echo "\${JENKINS_CREDS//%PRIVATE_KEY%/\$(<${sshKeyEnvVar})}" > ${JENKINS_CREDS_FILE}
             else
-                echo "bar"
-                sleep 1
                 echo "\${JENKINS_CREDS//%PRIVATE_KEY%/\$(<\${${sshKeyEnvVar}})}" > ${JENKINS_CREDS_FILE}
             fi
             
