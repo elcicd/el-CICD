04bae4fddc595fd6abeaabe2d02368e50e010dd0
hippyod <hippyod@yahoo.com>
2022-11-21 03:34:27 -0600

    programming error

--- vars/deploymentUtils.groovy
+++ vars/deploymentUtils.groovy
@@ -48,9 +48,9 @@ def deployMicroservices(def projectInfo, def components) {
                 set +e
                 if helm upgrade --atomic --install --history-max=1 \
                     --set-string ${msCommonValues.join(' --set-string ')} \
-                    \${VALUES_FILES} \
+                    \${VALUES_FILES} \\
                     -f ${el.cicd.CONFIG_HELM_DIR}/default-component-values.yaml \
-                    -f ${el.cicd.CONFIG_HELM_DIR}/component-meta-info-values.yaml \
+                    -f ${el.cicd.EL_CICD_HELM_DIR}/component-meta-info-values.yaml \
                     -n ${projectInfo.deployToNamespace} \
                     ${component.name} \
                     elCicdCharts/elCicdChart                    
@@ -65,7 +65,7 @@ def deployMicroservices(def projectInfo, def components) {
                     helm template --debug ${component.name} \
                         -f \${VALUES_FILE} \
                         --set-string elCicdChart.${msCommonValues.join(' --set-string elCicdChart.')} \
-                        -f ${el.cicd.EL_CICD_CONFIG_HELM_DIR}/default-component-values.yaml \
+                        -f ${el.cicd.CONFIG_HELM_DIR}/default-component-values.yaml \
                         -f ${el.cicd.EL_CICD_HELM_DIR}/component-meta-info-values.yaml \
                         -n ${projectInfo.deployToNamespace}
                     set -ex
