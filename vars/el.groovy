d529511650b91ae4e287d073578661878a4b7f53
hippyod <hippyod@yahoo.com>
2022-12-01 00:58:02 -0600

    testing

--- vars/el.groovy
+++ vars/el.groovy
@@ -69,7 +69,7 @@ def node(Map args, Closure body) {
         serviceAccount: "${el.cicd.JENKINS_SERVICE_ACCOUNT}",
         podRetention: onFailure(),
         idleMinutes: 30, //"${el.cicd.JENKINS_AGENT_MEMORY_IDLE_MINUTES}",
-        runAsUser: 1001,
+        runAsUser: 'jenkins',
         runAsGroup: 'root',
         containers: [
             containerTemplate(
