dcaa05243594faa3c6b1b4fa6f66e65f38ff06bc
hippyod <hippyod@yahoo.com>
2022-12-01 00:43:59 -0600

    testing

--- scripts/bootstrap-functions.sh
+++ scripts/bootstrap-functions.sh
@@ -228,9 +228,18 @@ __create_onboarding_automation_server() {
     set -x
     helm upgrade --wait --wait-for-jobs --install --history-max=1  \
         --set-string elCicdDefs.JENKINS_SYNC_JOB_IMAGE=${JENKINS_IMAGE_REGISTRY}/${JENKINS_AGENT_IMAGE_PREFIX}-${JENKINS_AGENT_DEFAULT} \
-        --set-string elCicdDefs.VOLUME_CAPACITY=${JENKINS_VOLUME_CAPACITY} \
         -n ${ONBOARDING_MASTER_NAMESPACE} \
         -f ${EL_CICD_HELM_DIR}/jenkins-pipeline-sync-job-values.yaml \
+        jenkins-pipeline-sync \
+        elCicdCharts/elCicdChart
+    set +ex
+    
+    echo
+    set -x
+    helm upgrade --install --history-max=1  \
+        --set-string elCicdDefs.VOLUME_CAPACITY=${JENKINS_VOLUME_CAPACITY} \
+        -n ${ONBOARDING_MASTER_NAMESPACE} \
+        -f ${EL_CICD_HELM_DIR}/jenkins-agent-pvc-values.yaml \
         jenkins-pipeline-sync \
         elCicdCharts/elCicdChart
     set +ex
