a0b7f8c3cc8e29cbe2dc0760f45e053550c2696d
hippyod <hippyod@yahoo.com>
2022-12-04 15:41:38 -0600

    testing

--- vars/el.groovy
+++ vars/el.groovy
@@ -70,6 +70,7 @@ def node(Map args, Closure body) {
         podRetention: onFailure(),
         idleMinutes: "${el.cicd.JENKINS_AGENT_MEMORY_IDLE_MINUTES}",
         yaml: '''
+          spec:
             securityContext:
               runAsNonRoot: true
               seccompProfile:
