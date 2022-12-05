70addd5ffd9b1b7b98eaf4355c21eb351b420c29
hippyod <hippyod@yahoo.com>
2022-12-04 20:32:22 -0600

    fixing permissions for nonroot-builder

--- vars/el.groovy
+++ vars/el.groovy
@@ -69,8 +69,8 @@ def node(Map args, Closure body) {
         serviceAccount: "${el.cicd.JENKINS_SERVICE_ACCOUNT}",
         podRetention: onFailure(),
         idleMinutes: 30, //"${el.cicd.JENKINS_AGENT_MEMORY_IDLE_MINUTES}",
-        runAsUser: '1001',
-        runAsGroup: '1001',
+        runAsUser: 'jenkins',
+        runAsGroup: '0',
         yaml: '''
           spec:
             securityContext:
