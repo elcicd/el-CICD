5333f5d27c1eb6747ae2e583586a50fa4d484462
hippyod <hippyod@yahoo.com>
2022-12-12 15:06:39 -0600

    testing

--- vars/onboardingUtils.groovy
+++ vars/onboardingUtils.groovy
@@ -94,9 +94,10 @@ def setupClusterWithProjectCicdResources(def projectInfo) {
         cat ${cicdConfigFile}
         
         ${shCmd.echo ''}            
-        helm upgrade --atomic --install --history-max=1 \
-            --set elCicdNamespaces='{${cicdNamespaces}'} \
+        helm template \
+            --set elCicdNamespaces='{${cicdNamespaces}}' \
             -f ${cicdConfigFile} \
+            -f ${el.cicd.CONFIG_HELM_DIR}/resource-quotas-values.yaml \
             -f ${el.cicd.CONFIG_HELM_DIR}/default-non-prod-cicd-values.yaml \
             -f ${el.cicd.EL_CICD_HELM_DIR}/non-prod-cicd-pipelines-values.yaml \
             -f ${el.cicd.EL_CICD_HELM_DIR}/non-prod-cicd-setup-values.yaml \
