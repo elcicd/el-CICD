3bc3f875374828467977ad056c6cf0036b3d3a23
hippyod <hippyod@yahoo.com>
2022-12-16 19:31:32 -0600

    adding in a oc wait option on uninstalls

--- vars/deploymentUtils.groovy
+++ vars/deploymentUtils.groovy
@@ -91,7 +91,7 @@ def createComponentRemovalStages(def projectInfo, def components) {
                     if [[ ! -z \$(helm list -q -n ${projectInfo.deployToNamespace} | grep ${component.name}) ]]
                     then
                         helm uninstall --wait ${component.name} -n ${projectInfo.deployToNamespace}
-                        oc wait --for=delete -l component=${component.name} --timeout=600s
+                        oc wait --for=delete pods -l component=${component.name} -n ${projectInfo.deployToNamespace} --timeout=600s
                     fi
                 """
             }
