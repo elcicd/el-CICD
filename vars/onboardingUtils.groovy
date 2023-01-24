6c04a83acaaa3c17501efbcb9d7228181d94a6ca
hippyod <hippyod@yahoo.com>
2023-01-24 17:40:00 -0600

    testing adding LimitRange support and removing default cpu limits

--- vars/onboardingUtils.groovy
+++ vars/onboardingUtils.groovy
@@ -74,7 +74,7 @@ def setupClusterWithProjectCicdResources(def projectInfo) {
         cat ${cicdConfigFile}
 
         ${shCmd.echo ''}
-        helm upgrade --atomic --install --history-max=1 --debug
+        helm upgrade --atomic --install --history-max=1 --debug \
             --set elCicdNamespaces='{${cicdNamespaces}}' \
             -f ${cicdConfigFile} \
             -f ${el.cicd.CONFIG_HELM_DIR}/default-non-prod-cicd-values.yaml \
