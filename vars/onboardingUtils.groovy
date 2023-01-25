18189f7359a6bd260337c54c67372e65c6e469a1
hippyod <hippyod@yahoo.com>
2023-01-24 18:57:43 -0600

    testing adding LimitRange support and removing default cpu limits

--- vars/onboardingUtils.groovy
+++ vars/onboardingUtils.groovy
@@ -92,7 +92,7 @@ def setupClusterWithProjectCicdResources(def projectInfo) {
         fi
 
         ${shCmd.echo ''}
-        helm upgrade --wait --wait-for-jobs --install --history-max=1 --debug \
+        helm upgrade --wait --wait-for-jobs --install --history-max=1 \
             --set-string elCicdDefs.JENKINS_SYNC_JOB_IMAGE=${baseAgentImage} \
             -f ${el.cicd.EL_CICD_HELM_DIR}/jenkins-pipeline-sync-job-values.yaml \
             -n ${projectInfo.cicdMasterNamespace} \
