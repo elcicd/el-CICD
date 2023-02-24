eb7a3e2fdd1163887accddef8cfa8644c07e3318
hippyod <hippyod@yahoo.com>
2023-02-24 00:33:16 -0600

    programming error

--- vars/deploymentUtils.groovy
+++ vars/deploymentUtils.groovy
@@ -111,7 +111,7 @@ def createComponentRemovalStages(def projectInfo, def components) {
 
 def createBuildStages(def projectInfo, def buildModules) {
     def buildStages = [failFast: true]
-    for (i in 0..< el.cicd.JENKINS_MAX_STAGES) {
+    for (i in 0..<(el.cicd.JENKINS_MAX_STAGES as int)) {
         def stageName = "parallel build stage ${i}"
         buildStages[stageName] = {
             stage(stageName) {
