4a6e4d5d7b532fe7758abf8951a9aed06f8c779f
hippyod <hippyod@yahoo.com>
2023-02-24 17:07:34 -0600

    testing

--- vars/el.groovy
+++ vars/el.groovy
@@ -155,7 +155,6 @@ def initializePipeline() {
             mkdir -p '${el.cicd.TEMP_DIR}'
             mkdir -p '${el.cicd.TEMPLATES_DIR}'
 
-            ${shCmd.echo 'OCP Runtime'}
             oc version
             ${shCmd.echo "\n=======================\n"}
             ${shCmd.echo 'Helm Version'}
