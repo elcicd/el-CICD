badc6da4a9122150f85c1f6990e730fec7dc2a6a
hippyod <hippyod@yahoo.com>
2023-04-01 17:47:54 -0500

    testing

--- vars/el.groovy
+++ vars/el.groovy
@@ -66,12 +66,14 @@ def node(Map args, Closure body) {
         volumeDefs += secretVolume(secretName: "${el.cicd.EL_CICD_BUILD_SECRETS_NAME}", mountPath: "${el.cicd.BUILDER_SECRETS_DIR}/")
     }
     
+    def serviceAccountName = args.isBuild ? el.cicd.JENKINS_IMAGE_BUILDER_SERVICE_ACCOUNT : el.cicd.JENKINS_SERVICE_ACCOUNT
+    
     podTemplate([
         label: "${args.agent}",
         cloud: 'openshift',
-        serviceAccount: "${el.cicd.JENKINS_SERVICE_ACCOUNT}",
+        serviceAccount: "${serviceAccountName}",
         podRetention: onFailure(),
-        idleMinutes: "${el.cicd.JENKINS_AGENT_MEMORY_IDLE_MINUTES}",
+        idleMinutes: "30", //"${el.cicd.JENKINS_AGENT_MEMORY_IDLE_MINUTES}",
         imagePullSecrets: ["el-cicd-jenkins-pull-secret"],
         yaml: '''
           spec:
