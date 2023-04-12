338d7982e84dc7a0b29aeef681cc24d7a0488eaf
hippyod <hippyod@yahoo.com>
2023-04-11 22:11:57 -0500

    programming error

--- .deploy/templateChart/kustomize.sh
+++ .deploy/templateChart/kustomize.sh
@@ -2,7 +2,7 @@
 set -e
 EL_CICD_KUSTOMIZE_DIR=${1}
 
-cat <&0 > ${EL_CICD_KUSTOMIZE_DIR}/helm-all.yaml
+cat <&0 > ./${EL_CICD_KUSTOMIZE_DIR}/helm-all.yaml
 
 kustomize edit add resource ${EL_CICD_KUSTOMIZE_DIR}/helm-all.yaml
 kustomize build ${EL_CICD_KUSTOMIZE_DIR}
