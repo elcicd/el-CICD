ba0c67236048385fd407349d8e0629e6bde65e0d
hippyod <hippyod@yahoo.com>
2023-04-23 02:07:34 -0500

    programming error

--- vars/onboardingUtils.groovy
+++ vars/onboardingUtils.groovy
@@ -283,19 +283,19 @@ def createCompSshKeyValues(def projectInfo) {
     projectInfo.modules.each { module ->
         dir(module.workDir) {
             echo "Creating deploy key for ${module.scmRepoName}"
-    
-            echo "module.scmDeployKeyJenkinsId.getClass: ${module.scmDeployKeyJenkinsId.getClass()}"
-            
-            def keygenScript = """
-                ssh-keygen -b 2048 -t rsa -f '${module.scmDeployKeyJenkinsId.toString()}' \
-                    -q -N '' -C 'el-CICD Component Deploy key' 2>/dev/null <<< y >/dev/null
-                
-                SSH_KEY=$(cat ${module.scmDeployKeyJenkinsId})
-                rm -f ${module.scmDeployKeyJenkinsId}
-                echo ${SSH_KEY}
-            """
 
-            def sshKey =  sh(returnStdout: true, script: keygenScript)
+            def scmDeployKeyJenkinsId = "${module.scmDeployKeyJenkinsId}"
+            def sshKey = 
+                sh(returnStdout: true,
+                   script: """
+                           ssh-keygen -b 2048 -t rsa -f '${scmDeployKeyJenkinsId}' \
+                               -q -N '' -C 'el-CICD Component Deploy key' 2>/dev/null <<< y >/dev/null
+                           
+                           SSH_KEY=$(cat ${scmDeployKeyJenkinsId})
+                           rm -f ${scmDeployKeyJenkinsId}
+                           echo ${SSH_KEY}
+                       """
+                   )
             
             cicdConfigValues["elCicdDefs-${module.scmDeployKeyJenkinsId}"] = ['SCM_REPO_SSH_KEY' : sshKey ]
         }
