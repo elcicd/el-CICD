14e0dce08cd3d698bcfa9ef69c950727f49c9c87
hippyod <hippyod@yahoo.com>
2023-05-29 17:30:48 -0500

    updaing project pv creation

--- vars/onboardingUtils.groovy
+++ vars/onboardingUtils.groovy
@@ -172,7 +172,9 @@ def getPvCicdConfigValues(def projectInfo) {
                 elCicdDefs.VOLUME_OBJ_NAMES << objName
 
                 volumeMap = [:]
-                volumeMap.CLAIM_NAME = pv.claimName
+                volumeMap.CLAIM_NAME = pv.claimName 
+                volumeMap.VOLUME_MODE = pv.volumeMode
+                volumeMap.RECLAIM_POLICY = pv.reclaimPolicy
                 volumeMap.STORAGE_CAPACITY = pv.capacity
                 volumeMap.ACCESS_MODES = pv.accessModes ? pv.accessModes : [pv.accessMode]
                 volumeMap.VOLUME_NAMESPACE = namespace
