88a8b52862056df7f48fd488e3ed44cfbfe39f0f
hippyod <hippyod@yahoo.com>
2023-10-22 17:29:03 -0500

    programming error

--- .deploy/templateChart/kustomize-comp.sh
+++ .deploy/templateChart/kustomize-comp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #!/bin/bash
-set -e
 PROFILES=${1}
 
 cd "$(dirname ${0})"
@@ -8,24 +7,17 @@ cd "$(dirname ${0})"
 mkdir -p ${elcicd_BASE_KUSTOMIZE_DIR} ${PROFILES}
 cat <&0 > ${elcicd_BASE_KUSTOMIZE_DIR}/${elcicd_PRE_KUST_HELM_FILE}
 
+set +e
 for OVERLAY_DIR in ${elcicd_BASE_KUSTOMIZE_DIR} ${PROFILES}
 do
-    cd ${OVERLAY_DIR}
+    (cd ${OVERLAY_DIR} && \
+        kustomize create --autodetect ${OVERLAY_DIR} 2>/dev/null && \
+        ([[ ! -z ../${LAST_OVERLAY_DIR} ]] && kustomize edit add resource "../${LAST_OVERLAY_DIR}")
+    )
     
-    set +e
-    kustomize create --autodetect ${OVERLAY_DIR} 2>/dev/null
-    set -e
-    
-    if [[ ! -z ${LAST_OVERLAY_DIR} ]]
-    then
-        kustomize edit add resource "../${LAST_OVERLAY_DIR}"
-    else
-        kustomize edit add resource ${elcicd_PRE_KUST_HELM_FILE}
-    fi
-
     LAST_OVERLAY_DIR=${OVERLAY_DIR}
-    cd ..
 done
+set -e
 
 kustomize build ${elcicd_POST_RENDERER_KUSTOMIZE_DIR} > ${elcicd_POST_KUST_HELM_FILE}
 
