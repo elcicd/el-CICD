d4ac9471ad5026924873cb5bf8570f7d507055b0
hippyod <hippyod@yahoo.com>
2023-11-05 21:36:33 -0600

    testing

--- vars/shCmd.groovy
+++ vars/shCmd.groovy
@@ -43,10 +43,13 @@ def tagImage(String env, String username, String pwd, String image, String fromT
 }
 
 def sshAgentBash(def sshKeyId, def ... commands) {
-    return "ssh-agent bash -c ' \
-        ssh-add -q \${${sshKeyId}} ;
-        git config user.name ${el.cicd.EL_CICD_ORGANIZATION} ;
-        ${commands.join('; ')}'" \
+    return " \
+        ssh-agent bash -c ' \
+            ssh-add -q \${${sshKeyId}} ; \
+            git config user.name ${el.cicd.EL_CICD_ORGANIZATION} ; \
+            ${commands.join('; ')} \
+        ' \
+    "
 }
 
 def echo(Object... msgs) {
