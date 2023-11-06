be216349d0313396c08a2cbeb902f2eeb1003b19
hippyod <hippyod@yahoo.com>
2023-11-06 02:47:27 -0600

    testing

--- .deploy/templateChart/kustomize-project.sh
+++ .deploy/templateChart/kustomize-project.sh
@@ -6,38 +6,36 @@ VARIANT=${2}
 
 cd "$(dirname "$0")"
 
-KUSTOMIZE=kustomize
-BASE=base
-EL_CICD_KUSTOMIZE=elcicd-kustomize
-KUSTOMIZATION=kustomization.yaml
-
 HELM_OUT_FILES_PREFIX=helmOut
 HELM_OUT_FILES_POSTFIX='%03d.yaml'
 HELM_OUT_DELIM='/---/'
-
-EL_CICD_COMP_ALL=elcicd-comp-all.yaml
 
 EL_CICD_DEPLOY_FINAL=elcicd-deploy-final.yaml
 
 set -e
 __kustomize_project() {
-    cat <&0 | csplit - \
+     cat <&0 | csplit - \
         --quiet \
         --prefix ${HELM_OUT_FILES_PREFIX} \
         --suffix-format ${HELM_OUT_FILES_POSTFIX} \
         --elide-empty-files ${HELM_OUT_DELIM} \
         '{*}'
 
-    rm ${EL_CICD_DEPLOY_FINAL}
+    rm -f ${EL_CICD_DEPLOY_FINAL}
     for COMP in $(ls -d charts/*/ | xargs -n 1 basename)
-    do
-        COMPDIR="./charts/${COMP}"
-        KUST_DIR=$(__find_and_create_kust_dir ${COMPDIR})        
-
-        kustomize build ${KUST_DIR} >> ${EL_CICD_DEPLOY_FINAL}
+    do  
+        KUST_DIR="./charts/${COMP}/kustomize"
+        
+        HELM_OUT_FILES=$(grep -l ${HELM_OUT_FILES_PREFIX}*.yaml -e "Source:.*/${COMP}/")
+        cat ${HELM_OUT_FILES} | ${KUST_DIR}/kustomize-comp.sh ${PROFILE} >> ${EL_CICD_DEPLOY_FINAL}
+        
+        rm ${HELM_OUT_FILES}
     done
+    rm -f ${HELM_OUT_FILES_PREFIX}*.yaml
     
     cat ${EL_CICD_DEPLOY_FINAL}
 }
+
+__kustomize_project
 
 set +e
