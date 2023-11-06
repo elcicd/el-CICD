f1a546265468ee3439de5bdf6757efd610bc2cc0
hippyod <hippyod@yahoo.com>
2023-11-05 21:26:25 -0600

    testing

--- vars/promoteProjectToProdUtils.groovy
+++ vars/promoteProjectToProdUtils.groovy
@@ -7,9 +7,6 @@ def verifyProjectReleaseVersion(def projectInfo) {
         versionTagScript = /git ls-remote ${projectInfo.projectModule.scmRepoUrl} '${projectInfo.releaseVersion}'/
         scmReleaseVersionBranch = sh(returnStdout: true, script: shCmd.sshAgentBash('GITHUB_PRIVATE_KEY', versionTagScript)).trim()
 
-        echo '=========='
-        echo "scmReleaseVersionBranch: ${scmReleaseVersionBranch}"
-        echo '=========='
         if (scmReleaseVersionBranch) {
             loggingUtils.errorBanner("RELEASE VERSION ${projectInfo.releaseVersion} HAS ALREADY BEEN PROMOTED")
         }
@@ -85,12 +82,12 @@ def createReleaseVersionUmbrellaChart(def projectInfo) {
             helm repo add elCicdCharts ${el.cicd.EL_CICD_HELM_REPOSITORY}
             helm template --set-string elCicdDefs.EL_CICD_MASTER_NAMESPACE=${projectInfo.teamInfo.cicdMasterNamespace} \
                           -f ${el.cicd.EL_CICD_TEMPLATE_CHART_DIR}/project-values.yaml \
-                          render-values-yaml elCicdCharts/elCicdChart | grep -vE '^(?:#|---)' > values.yaml
+                          render-values-yaml elCicdCharts/elCicdChart | grep -vG '^(?:#|---)' > values.yaml
 
             helm template --set-string elCicdDefs.VERSION=${projectInfo.releaseVersion} \
                           --set-string elCicdDefs.HELM_REPOSITORY_URL=${el.cicd.EL_CICD_HELM_REPOSITORY} \
                           -f ${el.cicd.EL_CICD_TEMPLATE_CHART_DIR}/helm-chart-yaml-values.yaml \
-                          ${projectInfo.id} elCicdCharts/elCicdChart | grep -vE '^(?:#|---)' > Chart.yaml
+                          ${projectInfo.id} elCicdCharts/elCicdChart | grep -vG '^(?:#|---)' > Chart.yaml
         """
     }
 }
