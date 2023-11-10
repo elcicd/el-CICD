0e29bf5a52b3b15e0284a57c057e5ee331bbf30e
hippyod <hippyod@yahoo.com>
2023-11-09 22:21:33 -0600

    programming error

--- vars/deployProjectToProdUtils.groovy
+++ vars/deployProjectToProdUtils.groovy
@@ -82,7 +82,7 @@ def selectReleaseVersion(def projectInfo, def args) {
     dir(projectInfo.projectModule.workDir) {
         sh """
             helm dependency update .
-            helm upgrade --install --atomic --cleanup-on-fail --history-max=2 --create-namespace \
+            helm upgrade --install --atomic --cleanup-on-fail --history-max=2 \
                     --set-string elCicdProfiles="{${postRendererArgs}}" \
                     --set-string elCicdDefs.PROD_ENV=${projectInfo.prodEnv} \
                     --post-renderer ./kustomize-project.sh \
