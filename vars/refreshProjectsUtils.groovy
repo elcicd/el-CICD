c8b9e53b29f43ddac35dee365c3a0c9ef6dafbe1
hippyod <hippyod@yahoo.com>
2023-11-26 17:00:58 -0600

    testing

--- vars/refreshProjectsUtils.groovy
+++ vars/refreshProjectsUtils.groovy
@@ -43,11 +43,10 @@ def removeUndeployedTeams(def projectRefreshMap) {
     echo "projectRefreshMap: ${projectRefreshMap}"
     echo "teamMasterNamespaces: ${teamMasterNamespaces}"
 
-    teamMasterNamespaces = sh(returnStdout: true, script: /
+    teamMasterNamespaces = sh(returnStdout: true, script: """
             oc get namespaces --ignore-not-found --no-headers -o name ${teamMasterNamespaces.join(' ')} | \
-                tr -d 'namespaces\/' | \
-                tr -d '-${el.cicd.EL_CICD_MASTER_NAMESPACE}'
-        /).split('\n')
+                sed -e 's|namespace/||' -e 's|-${el.cicd.EL_CICD_MASTER_NAMESPACE}||'
+        """).split('\n')
         
     echo "teamMasterNamespaces: ${teamMasterNamespaces}"
 
