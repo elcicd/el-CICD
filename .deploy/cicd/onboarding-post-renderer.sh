0d9869ec8b242338a63dfc1003a9868fbfe72b0d
hippyod <hippyod@yahoo.com>
2023-11-25 22:22:11 -0600

    first pass labeling all SDLC resources with project and team labels

--- .deploy/cicd/onboarding-post-renderer.sh
+++ .deploy/cicd/onboarding-post-renderer.sh
@@ -21,6 +21,6 @@ helm template --set-string PROJECT_ID=${PROJECT_ID},TEAM_ID=${TEAM_ID} \
 
 helm template --set-string PROJECT_ID=${PROJECT_ID},TEAM_ID=${TEAM_ID} \
     -f  onboarding-kustomization.yaml project-labels \
-    ${elcicd_EL_CICD_HELM_OCI_REGISTRY}/elcicd-chart > ${TMP_DIR}/kustomization.yaml
+    ${elcicd_EL_CICD_HELM_OCI_REGISTRY}/elcicd-chart | sed -E '/^#|^---/d' > ${TMP_DIR}/kustomization.yaml
     
 kustomize build ${TMP_DIR}
