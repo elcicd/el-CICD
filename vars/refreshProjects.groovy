543c3341f271da4269888e39d3038bc71ca117cb
hippyod <hippyod@yahoo.com>
2023-11-27 00:40:00 -0600

    programming error

--- vars/refreshProjects.groovy
+++ vars/refreshProjects.groovy
@@ -34,6 +34,7 @@ def call(Map args) {
     projectRefreshMap.each { teamId, projectList ->
         def teamInfo = projectInfoUtils.gatherTeamInfo(teamId)
         teamInfoList.add(teamInfo)
+        echo "teamInfo: ${teamInfo}"
         projectInfoList += projectList.collect { projectId ->
             return projectInfoUtils.gatherProjectInfoStage(teamInfo, projectId)
         }
