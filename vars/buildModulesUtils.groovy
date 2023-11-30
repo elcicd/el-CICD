bd7ff223c7857b58a7c16f40b37cd211996b20ed
hippyod <hippyod@yahoo.com>
2023-11-29 22:49:28 -0600

    programming error

--- vars/buildModulesUtils.groovy
+++ vars/buildModulesUtils.groovy
@@ -1,18 +1,18 @@
 
 
 def getSelectedModules(def projectInfo, def args) {
-    def DEPLOY_TO_NAMESPACE = 'Deploy Foo Namespace'
-    def GIT_BRANCH = 'Git branch?'
-    def CLEAN_NAMESPACE = 'Clean namespace?'
+    def DEPLOY_TO_NAMESPACE = 'DEPLOY_TO_NAMESPACE'
+    def GIT_BRANCH = 'GIT_BRANCH'
+    def CLEAN_NAMESPACE = 'CLEAN_NAMESPACE'
 
     def cnDesc = 'Uninstall all components currently deployed in selected namespace before deploying new builds'
     List inputs = [choice(name: DEPLOY_TO_NAMESPACE, description: 'The namespace to build and deploy to'),
                    stringParam(name: GIT_BRANCH, defaultValue: projectInfo.gitBranch, description: 'Branch to build?'),
                    booleanParam(name: CLEAN_NAMESPACE, description: cnDesc)]
 
-    def BUILD_ALL_ARTIFACTS = 'Build all artifacts'
-    def BUILD_ALL_COMPONENTS = 'Build and deploy all components'
-    def BUILD_ALL_TEST_MODULES = 'Build and deploy all test modules'
+    def BUILD_ALL_ARTIFACTS = 'BUILD_ALL_ARTIFACTS'
+    def BUILD_ALL_COMPONENTS = 'BUILD_ALL_COMPONENTS'
+    def BUILD_ALL_TEST_MODULES = 'BUILD_ALL_TEST_MODULES'
 
     inputs += booleanParam(name: BUILD_ALL_ARTIFACTS)
     inputs += booleanParam(name: BUILD_ALL_COMPONENTS)
@@ -29,9 +29,9 @@ def getSelectedModules(def projectInfo, def args) {
 
     def cicdInfo = jenkinsUtils.displayInputWithTimeout("Select artifacts and components to build:", args, inputs)
 
-    projectInfo.deployToNamespace = cicdInfo.get(DEPLOY_TO_NAMESPACE)
-    projectInfo.gitBranch = cicdInfo.get(GIT_BRANCH)
-    projectInfo.cleanNamespace = cicdInfo.get(CLEAN_NAMESPACE)
+    projectInfo.deployToNamespace = cicdInfo[DEPLOY_TO_NAMESPACE]
+    projectInfo.gitBranch = cicdInfo[GIT_BRANCH]
+    projectInfo.cleanNamespace = cicdInfo[CLEAN_NAMESPACE]
 
     projectInfo.selectedArtifacts = projectInfo.artifacts.collect { cicdInfo[BUILD_ALL_ARTIFACTS] || cicdInfo[it.name] }
     projectInfo.selectedComponents = projectInfo.components.collect { cicdInfo[BUILD_ALL_COMPONENTS] || cicdInfo[it.name] }
