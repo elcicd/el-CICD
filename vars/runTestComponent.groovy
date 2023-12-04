bd69c4f6de51d3e2ab3e4b26509c8018ea7a4bb0
hippyod <hippyod@yahoo.com>
2023-12-03 19:41:04 -0600

    programming error

--- vars/runTestComponent.groovy
+++ vars/runTestComponent.groovy
@@ -1,4 +1,4 @@
-/* 
+/*
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
  */
@@ -6,16 +6,15 @@
 void call(Map args) {
     def projectInfo = args.projectInfo
     def module = projectInfo.modules.find { it.name == args.moduleName }
-
     module.gitBranch = args.gitBranch
 
-    projectInfo.deployToEnv = projectInfo.testEnv
-    projectInfo.deployToNamespace = projectInfo.nonProdNamespaces(projectInfo.testEnv) ?: projectInfo.sandboxNamespaces(projectInfo.testEnv)
+    projectInfo.deployToEnv = args.testEnv
+    projectInfo.deployToNamespace = projectInfo.nonProdNamespaces[args.testEnv] ?: projectInfo.sandboxNamespaces[args.testEnv]
 
     stage('Checkout code from repository') {
         moduleUtils.cloneModule(module)
     }
-    
+
     [el.cicd.BUILDER, el.cicd.TESTER].each { buildStep ->
         stage("Test step: ${buildStep.toUpperCase()}") {
             moduleUtils.runBuildStep(projectInfo, module, buildStep, 'TEST MODULE')
