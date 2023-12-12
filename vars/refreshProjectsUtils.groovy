3080372d3821f4d823855751c03533b0f3ba4254
hippyod <hippyod@yahoo.com>
2023-12-11 23:56:02 -0600

    testing

--- vars/refreshProjectsUtils.groovy
+++ vars/refreshProjectsUtils.groovy
@@ -146,5 +146,6 @@ def removeUndeployedTeams(def projectRefreshMap) {
     teamNamespaceList = projectRefreshMap.keySet().collect { "${it}-${el.cicd.EL_CICD_MASTER_NAMESPACE}" }.join(' ')
     def namespaceScript = "oc get namespaces --ignore-not-found --no-headers ${teamNamespaceList} -o custom-columns=:metadata.name"
     projectNames = sh(returnStdout: true, script: namespaceScript).split("\n").collect { it - "-${el.cicd.EL_CICD_MASTER_NAMESPACE}" }
+    echo "projectNames: ${projectNames}"
     projectRefreshMap.keySet().retainAll(projectNames)    
 }
