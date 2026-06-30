888258b3070d9e5e310f81bec557a7fa0bb1efb7
hippyod <hippyod@yahoo.com>
2026-06-30 17:15:14 -0500

    bug fix

--- .deploy/cicd/onboarding-plugin/onboarding-post-renderer.sh
+++ .deploy/cicd/onboarding-plugin/onboarding-post-renderer.sh
@@ -15,10 +15,8 @@ mkdir ${TMP_DIR}
 
 cat <&0 > ${TMP_DIR}/${PROJECT_YAML}
 
-# helm template --set-string elCicdDefs.PROJECT_ID=${PROJECT_ID},elCicdDefs.TEAM_ID=${TEAM_ID} \
-#     -f onboarding-kustomization.yaml project-labels \
-#     ${elcicd_EL_CICD_HELM_OCI_REGISTRY}/elcicd-chart | sed -E '/^[#-]/d' > ${TMP_DIR}/kustomization.yaml
+helm template --set-string elCicdDefs.PROJECT_ID=${PROJECT_ID},elCicdDefs.TEAM_ID=${TEAM_ID} \
+    -f onboarding-kustomization.yaml project-labels \
+    ${elcicd_EL_CICD_HELM_OCI_REGISTRY}/elcicd-chart | sed -E '/^[#-]/d' > ${TMP_DIR}/kustomization.yaml
 
-# kustomize build ${TMP_DIR}
-
-cat ${TMP_DIR}/${PROJECT_YAML}
+kustomize build ${TMP_DIR}
